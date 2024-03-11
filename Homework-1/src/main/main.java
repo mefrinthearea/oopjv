163813105427b5eae1a6d82858e66b83091b6118
mefrinthearea <156604848+mefrinthearea@users.noreply.github.com>
2024-03-12 00:53:20 +0400

    Update main.java

--- Homework-1/src/main/main.java
+++ Homework-1/src/main/main.java
@@ -2,6 +2,11 @@ package Main;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Giorgi");
+        System.out.println("Giorgi"); 
+
+        //or like this 
+        
+        String name = "Giorgi";
+        System.out.println(name);
     }
 }
